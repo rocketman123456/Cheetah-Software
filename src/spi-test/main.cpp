52d229599d9a15dbc5df64dba1dbcd5e10e293c1
rocketman123456 <759094438@qq.com>
2023-04-15 14:30:18 +0800

    add abad zero control

--- src/spi-test/main.cpp
+++ src/spi-test/main.cpp
@@ -12,6 +12,16 @@ int main() {
     spi_command_drv.flags[2] = 1;
     spi_command_drv.flags[3] = 1;
 
+    spi_command_drv.kp_abad[0] = 10;
+    spi_command_drv.kp_abad[1] = 10;
+    spi_command_drv.kp_abad[2] = 10;
+    spi_command_drv.kp_abad[3] = 10;
+
+    spi_command_drv.kd_abad[0] = 0.5;
+    spi_command_drv.kd_abad[1] = 0.5;
+    spi_command_drv.kd_abad[2] = 0.5;
+    spi_command_drv.kd_abad[3] = 0.5;
+
     spi_driver_run();
 
     return 0;
