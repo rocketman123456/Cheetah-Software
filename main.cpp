a069322f5713c44733d61eca98e1850eeb0499e1
rocketman123456 <759094438@qq.com>
2023-05-11 21:29:26 +0800

    update

--- main.cpp
+++ main.cpp
@@ -19,8 +19,12 @@ int main()
     }
 
     transfer(spi_1_fd, tx, rx, 16);
-
     auto str = hex2str(rx, 16);
+    std::cout << "[SPI1]" << str << endl;
+
+    transfer(spi_2_fd, tx, rx, 16);
+    auto str = hex2str(rx, 16);
+    std::cout << "[SPI2]" << str << endl;
 
     spi_close();
 
