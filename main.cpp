7b76277588b935b142e44f3498e606a22e210ae8
rocketman123456 <759094438@qq.com>
2023-05-11 22:09:50 +0800

    update

--- main.cpp
+++ main.cpp
@@ -11,19 +11,20 @@ int main()
     spi_open();
 
     uint8_t tx[16];
-    uint8_t rx[16];
+    uint8_t rx_1[16];
+    uint8_t rx_2[16];
 
     for(int i = 0; i < 16; ++i)
     {
         tx[i] = i;
     }
 
-    transfer(spi_1_fd, tx, rx, 16);
-    auto str = hex2str(rx, 16);
+    transfer(spi_1_fd, tx, rx_1, 16);
+    auto str = hex2str(rx_1, 16);
     std::cout << "[SPI1]" << str << std::endl;
 
-    transfer(spi_2_fd, tx, rx, 16);
-    str = hex2str(rx, 16);
+    transfer(spi_2_fd, tx, rx_2, 16);
+    str = hex2str(rx_2, 16);
     std::cout << "[SPI2]" << str << std::endl;
 
     spi_close();
